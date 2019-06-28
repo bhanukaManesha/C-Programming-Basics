c62aa9dd2e4dacfd2024e9585237c4cae3970c19
bhanukaManesha <bhanukamgamage@gmail.com>
2019-06-28 08:52:27 +0800

    Refactored and Started Section 5

---
+++ Section 5/overview.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+# include <stdbool.h>
+
+int main(){
+    int bhanuka = 30;
+    
+    // Using the classical definition
+    _Bool myOldBool = true;
+
+    // Using the standard boolen library
+    bool myBool = true;
+
+    return 0;
+}
