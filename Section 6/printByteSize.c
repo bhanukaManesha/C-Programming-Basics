c023b24aa0ab5f0b6c991f3d0afb94b8fceeadcc
bhanukaManesha <bhanukamgamage@gmail.com>
2019-06-29 10:48:13 +0800

    sizeof

---
+++ Section 6/printByteSize.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("The size of int : %zd \n",sizeof(int));
+    printf("The size of char : %zd \n",sizeof(char));
+    printf("The size of long : %zd \n",sizeof(long));
+    printf("The size of long long : %zd \n",sizeof(long long));
+    printf("The size of double : %zd \n",sizeof(double));
+    printf("The size of long double : %zd \n",sizeof(long double));
+
+
+    return 0;
+}
